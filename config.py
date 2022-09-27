c4459190907a388fa1032534d3377d3ecca6e64f
chonghuixin <87887559+chonghuixin@users.noreply.github.com>
2022-09-27 13:12:03 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "yeechooili-employee.cuf0jxtawdje.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "aws_user"
+customdb = "yeechooiliEmp"
+custombucket = "yeechooili-bucket"
+customregion = "us-east-1"
